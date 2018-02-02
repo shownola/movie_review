7f15d5fb1954d68746fd160096e8c201cace131e
SHERRY <swasieko@gmail.com>
2018-02-02 21:02:57 +0000

    styled index and show pages add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :reviews
   devise_for :users
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: "home#index"
+  root "movies#index"
 end
